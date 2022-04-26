aebe99bb422fc3c89793344e9dacc0c99f51cbb2
Laura Powles <laura@laurapowles.com>
2022-04-26 15:18:23 +1000

    replaced BoundedU32 with WrappingU32
    Signed-off-by: Laura Powles <laura@laurapowles.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,57 @@
 use std::ops::Add;
-use std::ops::Deref;
-use std::ops::Div;
-use std::ops::Mul;
-use std::ops::Rem;
-use std::ops::Sub;
+use std::ops::AddAssign;
 
-pub struct BoundedU32<const MIN: u32, const MAX: u32>(u32);
+#[derive(Debug, Clone, Copy)]
+pub struct WrappingU32<const MIN: u32, const MAX: u32>(u32);
 
-impl<const MIN: u32, const MAX: u32> BoundedU32<MIN, MAX> {
-    pub fn new(inner: u32) -> Self {
+impl<const MIN: u32, const MAX: u32> WrappingU32<MIN, MAX> {
+    pub fn new(mut inner: u32) -> Self {
+        if inner > MAX {
+            let rem = (inner - MIN) % (MAX - MIN);
+            inner = rem + MIN;
+        }
+
         Self(inner)
     }
 
-    pub fn is_valid(&self) -> bool {
-        self.0 >= MIN && self.0 < MAX
+    pub fn inner(&self) -> u32 {
+        self.0
     }
 }
 
-impl<const MIN: u32, const MAX: u32> From<u32> for BoundedU32<MIN, MAX> {
+impl<const MIN: u32, const MAX: u32> From<u32> for WrappingU32<MIN, MAX> {
     fn from(inner: u32) -> Self {
         Self(inner)
     }
 }
 
-impl<const MIN: u32, const MAX: u32> Add<u32> for BoundedU32<MIN, MAX> {
-    type Output = Result<BoundedU32<MIN, MAX>, ArithmeticError>;
+impl<const MIN: u32, const MAX: u32> Add<u32> for WrappingU32<MIN, MAX> {
+    type Output = u32;
     fn add(self, other: u32) -> Self::Output {
-        let result = BoundedU32::from(self.0 + other);
-        if result.is_valid() {
-            Ok(result)
-        } else {
-            Err(ArithmeticError::WouldOverflow)
-        }
+        self.0 + other
+    }
+}
+
+impl<const MIN: u32, const MAX: u32, const OTHER_MIN: u32, const OTHER_MAX: u32>
+    Add<WrappingU32<OTHER_MIN, OTHER_MAX>> for WrappingU32<MIN, MAX>
+{
+    type Output = u32;
+    fn add(self, other: WrappingU32<OTHER_MIN, OTHER_MAX>) -> Self::Output {
+        self.0 + other.0
+    }
+}
+
+impl<const MIN: u32, const MAX: u32> AddAssign<u32> for WrappingU32<MIN, MAX> {
+    fn add_assign(&mut self, other: u32) {
+        *self = WrappingU32::from(*self + other);
+    }
+}
+
+impl<const MIN: u32, const MAX: u32, const OTHER_MIN: u32, const OTHER_MAX: u32>
+    AddAssign<WrappingU32<OTHER_MIN, OTHER_MAX>> for WrappingU32<MIN, MAX>
+{
+    fn add_assign(&mut self, other: WrappingU32<OTHER_MIN, OTHER_MAX>) {
+        *self += other.0
     }
 }
 
@@ -47,15 +67,21 @@ mod tests {
 
     #[test]
     fn can_create() {
-        let bounded = BoundedU32::<3, 9>(5);
-        assert!(bounded.is_valid());
+        let bounded = WrappingU32::<3, 9>(5);
+        assert_eq!(bounded.inner(), 5)
     }
 
     #[test]
-    fn can_add() {
-        let bounded = BoundedU32::<3, 9>(5);
-        let result = bounded + 2;
-        assert!(result.is_ok());
-        assert_eq!(result.unwrap().0, 7);
+    fn can_add_int() {
+        let bounded = WrappingU32::<3, 9>(4);
+        assert_eq!(bounded + 2, 6);
+    }
+
+    #[test]
+    fn can_add_other() {
+        let a = WrappingU32::<3, 9>(3);
+        let b = WrappingU32::<1, 302>(4);
+        assert_eq!(a + b, 7);
+        assert_eq!(b + a, 7);
     }
 }
