f2b1c291665eace15d52fffceb179a7b40a189b5
Laura Powles <laura@laurapowles.com>
2022-05-06 20:28:59 +1000

    added examples to the crate-level docs

--- src/lib.rs
+++ src/lib.rs
@@ -34,6 +34,55 @@
 //!   - Cheaper and more ergonimic.
 //!   - Bounds are `const` and must be set at compile time.
 //!   - Only available for integers.
+//!
+//! # Examples
+//!
+//! Bounded types:
+//! ```rust
+//! use clamps::bounded::{BoundedU32, BoundsError};
+//!
+//! let bounded = BoundedU32::<3, 7>::try_from(5);
+//! assert_eq!(bounded.unwrap(), 5);
+//!
+//! let invalid = BoundedU32::<3, 7>::try_from(10);
+//! assert_eq!(invalid, Err(BoundsError::TooLarge));
+//! ```
+//!
+//! Wrapping types:
+//! ```rust
+//! use clamps::wrapping::WrappingU32;
+//!
+//! let mut wrapping = WrappingU32::<2, 8>::from(5);
+//! assert_eq!(wrapping, 5);
+//! assert_eq!(wrapping + 5, 10);
+//!
+//! // 5 + 5 (bounds: 2..8) = 4
+//! wrapping += 5;
+//! assert_ne!(wrapping, 10);
+//! assert_eq!(wrapping, 4);
+//!
+//! // 4 - 3 (bounds: 2..8) = 7
+//! wrapping -= 3;
+//! assert_ne!(wrapping, 1);
+//! assert_eq!(wrapping, 7);
+//! ```
+//!
+//! Saturating types:
+//! ```rust
+//! use clamps::saturating::SaturatingU32;
+//!
+//! let mut saturating = SaturatingU32::<5, 10>::from(8);
+//! assert_eq!(saturating, 8);
+//! assert_eq!(saturating + 5, 13);
+//!
+//! // 8 + 100 (bounds: 5..=10) = 10
+//! saturating += 100;
+//! assert_eq!(saturating, 10);
+//!
+//! // 10 - 100 (bounds: 5..=10) = 5
+//! saturating -= 100;
+//! assert_eq!(saturating, 5);
+//! ```
 
 
 pub mod bounded;
